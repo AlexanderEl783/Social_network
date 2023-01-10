e5e420834bf0fbac0366c5169ca5beebc8dd078d
Alexander <9254017@gmail.com>
2023-01-10 12:57:16 +0500

    2 commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './Components/Header';
+import Navbar from './Components/Navbar';
+import Content from './Components/Content';
 
 
 const App = () => {
   return (
     <div className='app-wrapper'>
+      <Header />
+      <Navbar />
+      <Content />
     </div>
   );
 }
