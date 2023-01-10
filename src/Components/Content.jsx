c0226c6369117a83b011eb0e411de2e5a67ea93d
Alexander <9254017@gmail.com>
2023-01-10 12:56:13 +0500

    start-project

---
+++ src/Components/Content.jsx
@@ -0,0 +1,29 @@
+
+const Content = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCZlf5lc5tX-0gY-y94pGS0mQdL-D0lCH2OQ&usqp=CAU' />
+            </div>
+            <div>
+                ava +descr
+            </div>
+            <div>
+                my posts
+                <div>
+                    new post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Content;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI05d0VUtdygXuHVKOxN-gnKCAsOu-1ZKN7Q&usqp=CAU' />
+        </header>
+    )
+}
+
+export default Header;
