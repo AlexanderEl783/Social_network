7e6a7221015bbb1c02b7e3a0d3a4d7f075d48743
Alexander <9254017@gmail.com>
2023-01-13 12:47:51 +0500

    22 - added links to dialogs

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,14 +1,25 @@
+import { NavLink } from 'react-router-dom';
 import classes from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
-                <div className={`${classes.dialog} ${classes.active}`}>Alex</div>
-                <div className={classes.dialog}>John</div>
-                <div className={classes.dialog}>Lucy</div>
-                <div className={classes.dialog}>Tom</div>
-                <div className={classes.dialog}>Dwain</div>
+                <div className={`${classes.dialog} ${classes.active}`}>
+                    <NavLink to='/dialogs/1'>Alex</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to='/dialogs/2'>John</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to='/dialogs/3'>Lucy</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to='/dialogs/4'>Tom</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to='/dialogs/5'>Dwain</NavLink>
+                </div>
             </div>
             <div className={classes.messages}>
                 <div className={classes.message}>Hello</div>
