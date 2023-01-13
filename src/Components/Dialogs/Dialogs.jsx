949ab6bea54c713ed47c7c298180ecd289a633ff
Alexander <9254017@gmail.com>
2023-01-13 12:15:53 +0500

    21 - start working with messages

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -2,8 +2,19 @@ import classes from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={`${classes.dialog} ${classes.active}`}>Alex</div>
+                <div className={classes.dialog}>John</div>
+                <div className={classes.dialog}>Lucy</div>
+                <div className={classes.dialog}>Tom</div>
+                <div className={classes.dialog}>Dwain</div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hello</div>
+                <div className={classes.message}>How are you?</div>
+                <div className={classes.message}>Yo</div>
+            </div>
         </div>
     )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import classes from './Navbar.module.css';
 import { NavLink } from 'react-router-dom';
 
+// присвоение класса при нажатии на ссылку
 const setActive = (navData) => navData.isActive ? classes.active : classes.item;
 
 const Navbar = () => {
